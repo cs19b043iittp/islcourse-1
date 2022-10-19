10049341f7778dd73e6a4a73241263f137b3e1ab
cs19b001iittp <59331608+cs19b001iittp@users.noreply.github.com>
2022-10-18 22:09:05 -0700

    First commit of exam

--- hubconf.py
+++ hubconf.py
@@ -6,9 +6,20 @@ def kali():
   
 # Define a neural network YOUR ROLL NUMBER (all small letters) should prefix the classname
 class cs19b001NN(nn.Module):
-  pass
-  # ... your code ...
-  # ... write init and forward functions appropriately ...
+  def __init__(self):
+        super(NeuralNetwork, self).__init__()
+        self.flatten = nn.Flatten()
+        self.linear_relu_stack = nn.Sequential(
+            nn.Linear(28*28, 512),
+            nn.ReLU(),
+            nn.Linear(512, 512),
+            nn.ReLU(),
+            nn.Linear(512, 10)
+        )
+   def forward(self, x):
+        x = self.flatten(x)
+        logits = self.linear_relu_stack(x)
+        return logits
     
 # sample invocation torch.hub.load(myrepo,'get_model',train_data_loader=train_data_loader,n_epochs=5, force_reload=True)
 def get_model(train_data_loader=None, n_epochs=10):
