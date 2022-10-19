a9bb208657346c810ad49b63b1b40590127f41d6
pandaking <aditya9660sharma@gmail.com>
2022-10-19 11:14:55 +0530

    changed code structure, add loss function

--- hubconf.py
+++ hubconf.py
@@ -7,12 +7,17 @@ from PIL import Image
 
 device = "cpu"
 
-def kali():
-  print ('kali')
-  
+loss_function = nn.CrossEntropyLoss()
+
+
+def adi():
+    print('adi')
+
 # Define a neural network YOUR ROLL NUMBER (all small letters) should prefix the classname
+
+
 class cs19b001NN(nn.Module):
-  def __init__(self):
+    def __init__(self):
         super(NeuralNetwork, self).__init__()
         self.flatten = nn.Flatten()
         self.linear_relu_stack = nn.Sequential(
@@ -22,7 +27,8 @@ class cs19b001NN(nn.Module):
             nn.ReLU(),
             nn.Linear(512, 10)
         )
-  def forward(self, x):
+
+    def forward(self, x):
         x = self.flatten(x)
         logits = self.linear_relu_stack(x)
         return logits
@@ -30,71 +36,75 @@ class cs19b001NN(nn.Module):
 
 # sample invocation torch.hub.load(myrepo,'get_model',train_data_loader=train_data_loader,n_epochs=5, force_reload=True)
 def get_model(train_data_loader=None, n_epochs=10):
-  model = None
+    model = None
 
-  model = NeuralNetwork().to(device)
+    model = NeuralNetwork().to(device)
 
-  # write your code here as per instructions
-  # ... your code ...
-  # ... your code ...
-  # ... and so on ...
-  # Use softmax and cross entropy loss functions
-  # set model variable to proper object, make use of train_data
-  
-  print ('Returning model... (rollnumber: xx)')
-  
-  return model
+    # write your code here as per instructions
+    # ... your code ...
+    # ... your code ...
+    # ... and so on ...
+    # Use softmax and cross entropy loss functions
+    # set model variable to proper object, make use of train_data
+
+    print('Returning model... (rollnumber: cs19b001)')
+
+    return model
 
 # sample invocation torch.hub.load(myrepo,'get_model_advanced',train_data_loader=train_data_loader,n_epochs=5, force_reload=True)
-def get_model_advanced(train_data_loader=None, n_epochs=10,lr=1e-4,config=None):
-  model = None
 
-  # write your code here as per instructions
-  # ... your code ...
-  # ... your code ...
-  # ... and so on ...
-  # Use softmax and cross entropy loss functions
-  # set model variable to proper object, make use of train_data
-  
-  # In addition,
-  # Refer to config dict, where learning rate is given, 
-  # List of (in_channels, out_channels, kernel_size, stride=1, padding='same')  are specified
-  # Example, config = [(1,10,(3,3),1,'same'), (10,3,(5,5),1,'same'), (3,1,(7,7),1,'same')], it can have any number of elements
-  # You need to create 2d convoution layers as per specification above in each element
-  # You need to add a proper fully connected layer as the last layer
-  
-  # HINT: You can print sizes of tensors to get an idea of the size of the fc layer required
-  # HINT: Flatten function can also be used if required
-  return model
-  
-  
-  print ('Returning model... (rollnumber: xx)')
-  
-  return model
+
+def get_model_advanced(train_data_loader=None, n_epochs=10, lr=1e-4, config=None):
+    model = None
+
+    # write your code here as per instructions
+    # ... your code ...
+    # ... your code ...
+    # ... and so on ...
+    # Use softmax and cross entropy loss functions
+    # set model variable to proper object, make use of train_data
+
+    # In addition,
+    # Refer to config dict, where learning rate is given,
+    # List of (in_channels, out_channels, kernel_size, stride=1, padding='same')  are specified
+    # Example, config = [(1,10,(3,3),1,'same'), (10,3,(5,5),1,'same'), (3,1,(7,7),1,'same')], it can have any number of elements
+    # You need to create 2d convoution layers as per specification above in each element
+    # You need to add a proper fully connected layer as the last layer
+
+    # HINT: You can print sizes of tensors to get an idea of the size of the fc layer required
+    # HINT: Flatten function can also be used if required
+
+
+    print('Returning model... (rollnumber: xx)')
+
+    return model
 
 # sample invocation torch.hub.load(myrepo,'test_model',model1=model,test_data_loader=test_data_loader,force_reload=True)
+
+
 def test_model(model1=None, test_data_loader=None):
 
-  accuracy_val, precision_val, recall_val, f1score_val = 0, 0, 0, 0
-  # write your code here as per instructions
-  # ... your code ...
-  # ... your code ...
-  # ... and so on ...
-  # calculate accuracy, precision, recall and f1score
+    accuracy_val, precision_val, recall_val, f1score_val = 0, 0, 0, 0
+    # write your code here as per instructions
+    # ... your code ...
+    # ... your code ...
+    # ... and so on ...
+    # calculate accuracy, precision, recall and f1score
 
-  size = len(test_data_loader.dataset)
-  num_batches = len(test_data_loader)
-  model1.eval()
-  test_loss, correct = 0, 0
-  with torch.no_grad():
+    size = len(test_data_loader.dataset)
+    num_batches = len(test_data_loader)
+    model1.eval()
+    test_loss, correct = 0, 0
+
+    with torch.no_grad():
         for X, y in test_data_loader:
             X, y = X.to(device), y.to(device)
             pred = model1(X)
-            test_loss += loss_fn(pred, y).item()
+            test_loss += loss_function(pred, y).item()
             correct += (pred.argmax(1) == y).type(torch.float).sum().item()
-  test_loss /= num_batches
-  correct /= size
-  
-  print ('Returning metrics... (rollnumber: xx)')
-  
-  return accuracy_val, precision_val, recall_val, f1score_val
+    test_loss /= num_batches
+    correct /= size
+
+    print('Returning metrics... (rollnumber: cs19b001)')
+
+    return accuracy_val, precision_val, recall_val, f1score_val
